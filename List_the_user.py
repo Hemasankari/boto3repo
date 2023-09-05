b12913402fdef29661a0dad26c7cb61065ff06c0
Hemasankari <144112308+Hemasankari@users.noreply.github.com>
2023-09-05 14:51:16 +0530

    Update List_the_user.py

--- List_the_user.py
+++ List_the_user.py
@@ -1,6 +1,6 @@
 import boto3
 
-client_var = boto3.client('iam',aws_access_key_id="AKIAYTCSR333HP7F24FM",aws_secret_access_key="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")
+client_var = boto3.client('iam',aws_access_key_id="AKIASTPYRH4UKPVZGJKW",aws_secret_access_key="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")
 
 
 
